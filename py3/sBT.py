dc77de2a2cb272f839be75ac2ef99e605e08b5ec
Mike Fiorino <mfiorino@gmu.edu>
2025-08-19 15:57:00 +0000

    py3 version of sbt

---
+++ py3/sBT.py
@@ -0,0 +1,7 @@
+# -- local vars
+from sbtLocal import *
+# -- global vars
+from sBTvars import *
+from sBTvm import *
+from sBTcl import *
+
---
+++ py3/sbtLocal.py
@@ -0,0 +1,5 @@
+# -- full path with local installation directories
+#
+sbtRoot='/sbt/superBT-V04'
+sbtRootLocal = '/sbt/local'
+sbtVersion='V04'
